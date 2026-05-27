08ab2b57dbf831a5a467aeb09ebc1ec3851ff1cf
arracheille <arachelrabbani@gmail.com>
2026-05-28 00:14:22 +0700

    reversing changes by vercel deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,20 +6,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
-// 1. Buat folder sementara di Vercel
-$tmpDirs = [
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache/data',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/logs',
-    '/tmp/storage/app',
-];
-foreach ($tmpDirs as $dir) {
-    if (!is_dir($dir)) mkdir($dir, 0755, true);
-}
-
-// 2. Buat aplikasinya
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -34,8 +21,3 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
-// 3. Pasang folder penyimpanan ke /tmp yang sudah dibuat
-$app->useStoragePath('/tmp/storage');
-
-return $app;
