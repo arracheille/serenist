b45af56b1e30e183527df34d41f1f0dee30ce0ca
arracheille <arachelrabbani@gmail.com>
2026-05-27 23:50:00 +0700

    fixing vercel deploy 4

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,24 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+// Tambahkan ini:
+$tmpDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/storage/app',
+];
+foreach ($tmpDirs as $dir) {
+    if (!is_dir($dir)) mkdir($dir, 0755, true);
+}
+
+$app->useStoragePath('/tmp/storage');
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
