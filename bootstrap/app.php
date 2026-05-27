38754938f3a61b162789dececa4afc7b3c6032e5
arracheille <arachelrabbani@gmail.com>
2026-05-28 00:01:11 +0700

    vercel deploy fix 6

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
-$app = new Illuminate\Foundation\Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
-
-// Tambahkan ini:
+// 1. Buat folder sementara di Vercel
 $tmpDirs = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache/data',
@@ -22,9 +18,8 @@ foreach ($tmpDirs as $dir) {
     if (!is_dir($dir)) mkdir($dir, 0755, true);
 }
 
-$app->useStoragePath('/tmp/storage');
-
-return Application::configure(basePath: dirname(__DIR__))
+// 2. Buat aplikasinya
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -39,3 +34,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// 3. Pasang folder penyimpanan ke /tmp yang sudah dibuat
+$app->useStoragePath('/tmp/storage');
+
+return $app;
